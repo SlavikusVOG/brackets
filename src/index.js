e5ead87233485fc50893068664abcd4262169b4d
SlavikusVOG <slavaolish@gmail.com>
2022-03-01 01:36:08 +0300

    fet: wrote function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketsToCheck = [];
+    const strArray = str.split("");
+    let lostFlag;
+    strArray.forEach((bracket) => {
+        const found = bracketsConfig.find((config) => {
+            if (
+                bracketsToCheck.length > 0 
+                && bracket === bracketsToCheck[bracketsToCheck.length-1][1]
+            ) {
+                bracketsToCheck.pop();
+                return true;
+            }
+            else if (bracket === config[0]) {
+                bracketsToCheck.push(config);
+                return true;
+            }
+            return false;
+        });
+        if (!found) {
+            lostFlag = true;
+        }
+    })
+    return bracketsToCheck.length > 0 || lostFlag ? false : true;
 }
